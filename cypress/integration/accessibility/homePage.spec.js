f1bbe4140e05907998190d964b0bf256998a9579
Nick Buonincontri <nbuonin@gmail.com>
2019-07-30 17:10:14 -0400

    OPIOIDS-152 Add nav element to test coverage, fix issue
    This commit sets Axe to open a nav item first before running checks. The
    tool won't test elements that are not visible, so this check insures
    that its caught.
    
    This commit also reworks the nav dropdown so elements are correctly
    labeled.

--- cypress/integration/accessibility/homePage.spec.js
+++ cypress/integration/accessibility/homePage.spec.js
@@ -1,5 +1,8 @@
 it('Tests the home page', function() {
     cy.visit('http://localhost:1313');
     cy.injectAxe();
+
+    // Open a nav item first, Axe won't check non-visible elements
+    cy.get('.dropdown-toggle').first().click();
     cy.checkA11y();
 });

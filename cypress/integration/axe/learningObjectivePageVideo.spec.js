34e11e9af7f2a72e15a6a90e11459c88d0449b5d
Nick Buonincontri <nbuonin@gmail.com>
2019-07-31 16:54:19 -0400

    OPIOID-152 Fix issues with learning objective pages

---
+++ cypress/integration/axe/learningObjectivePageVideo.spec.js
@@ -0,0 +1,8 @@
+it('Tests the top level leaerning objective page template', function() {
+    cy.visit('http://localhost:1313/domains/identify/screen-for-substance-disorders/video/');
+    cy.injectAxe();
+
+    // Open a nav item first, Axe won't check non-visible elements
+    cy.get('.dropdown-toggle').first().click();
+    cy.checkA11y();
+});

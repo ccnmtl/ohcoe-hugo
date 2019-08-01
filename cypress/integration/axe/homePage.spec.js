6fed2f7d6d16777a13be12895f82cda499c796af
Nick Buonincontri <nbuonin@gmail.com>
2019-08-01 11:01:26 -0400

    OPIOID-152 Add coverage to reset modal

--- cypress/integration/axe/homePage.spec.js
+++ cypress/integration/axe/homePage.spec.js
@@ -6,3 +6,12 @@ it('Tests the home page', function() {
     cy.get('.dropdown-toggle').first().click();
     cy.checkA11y();
 });
+
+it('Tests the reset modal', function() {
+    cy.visit('http://localhost:1313');
+    cy.injectAxe();
+
+    // Open a nav item first, Axe won't check non-visible elements
+    cy.get('#reset-trigger').click();
+    cy.checkA11y();
+});

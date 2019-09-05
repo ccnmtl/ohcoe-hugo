04bb206f77586f151e6062bf68a693c62391c603
Nick Buonincontri <nbuonin@gmail.com>
2019-09-05 14:43:45 -0400

    Video Analytics: test that role/specialty are captured by analytic

--- cypress/integration/analytics.spec.js
+++ cypress/integration/analytics.spec.js
@@ -26,17 +26,4 @@ describe('Google Analytics Custom Event tests', () => {
             );
         });
     });
-    it('plays a video and checks that gtag is called on click', function() {
-        cy.visit('http://localhost:1313/domains/identify/screen-for-substance-disorders/video/').then((win) => {
-            win.gtag = cy.stub().as('gtag');
-        }).then((win) => {
-            // click the video, wait 3 seconds, click another link, and assert the test
-            win.videoTrackers['ytplayer-A9aLIXkf05Y'].videoStart(0.00);
-            win.videoTrackers['ytplayer-A9aLIXkf05Y'].videoStop(3.45);
-            cy.get('.input-group > .btn').contains('Next').click().then(() => {
-                cy.get('@gtag').should('be.called');
-            });
-        });
-    });
-
 });
